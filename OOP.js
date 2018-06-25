d49a14f26dff8f1740c65e97dd560473565b4153
Maxim <maxrap7878@mail.ru>
2018-06-25 13:40:03 +0300

    add event onmouseup

--- OOP.js
+++ OOP.js
@@ -1,5 +1,3 @@
-
-
 class Plant {
    constructor(name){
        this.name = name;
@@ -10,10 +8,40 @@ class Plant {
 
 }
 
-let Tomato = new Plant("Tomato");
-console.log(Tomato.lvl);
+let tomato = new Plant("Tomato");
+console.log(tomato.lvl);
+
 class Water {
 constructor(){
-g
+ this.waterImage = document.getElementById('water');
+}
+getWaterPosition (){
+    let waterImage = document.getElementById('water');
+    let position = waterImage.getBoundingClientRect();
+    return{
+        top: position.top,
+        left: position.left,
+    }
+}
+setWaterPosition(e){
+    console.log('it works');
+    let posX = e.clientX ;
+    let posY = e.clientY ;
+    let waterImage = document.getElementById('water');
+    let position = waterImage.getBoundingClientRect();
+    console.log(posX);
+    console.log(posY);
+    console.log(waterImage.style.left);
+    console.log(waterImage.style.top);
+
+
+    waterImage.style.left = `${posX - waterImage.offsetWidth/4}px`;
+    waterImage.style.top = `${posY  - waterImage.offsetHeight/4}px`;
 }
 }
+let water = new Water();
+console.log(water.waterImage);
+console.log(water.getWaterPosition());
+console.log(water.getWaterPosition().left);
+
+water.waterImage.addEventListener('mousedown', water.setWaterPosition);
