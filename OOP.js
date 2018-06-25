b8fd19464e0c8b2edd7d0c8aa00ff330fe56dfbc
Maxim <maxrap7878@mail.ru>
2018-06-25 11:07:58 +0300

    add images

---
+++ OOP.js
@@ -0,0 +1,19 @@
+
+
+class Plant {
+   constructor(name){
+       this.name = name;
+       this.watered = false;
+       this.lvl = 0;
+
+   }
+
+}
+
+let Tomato = new Plant("Tomato");
+console.log(Tomato.lvl);
+class Water {
+constructor(){
+g
+}
+}
